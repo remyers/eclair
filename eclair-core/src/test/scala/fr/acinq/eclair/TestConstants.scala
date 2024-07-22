5eb4cfebaa670bc731c6ba9f24b83939e27cbef6
t-bast <bastien@acinq.fr>
2024-07-22 16:00:01 +0200

    Use official splice messages
    We replace our experimental version of `splice_init`, `splice_ack` and
    `splice_locked` by their official version. If our peer is using the
    experimental feature bit, we convert our outgoing messages to use the
    experimental encoding and incoming messages to the official messages.
    
    We also change the TLV fields added to `tx_add_input`, `tx_signatures`
    and `splice_locked` to match the spec version. We always write both the
    official and experimental TLV to updated nodes (because the experimental
    one is odd and will be ignored) but we drop the official TLV if our
    peer is using the experimental feature, because it won't understand the
    even TLV field.
    
    This guarantees backwards-compatibility with peers who only support the
    experimental feature.

--- eclair-core/src/test/scala/fr/acinq/eclair/TestConstants.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/TestConstants.scala
@@ -108,7 +108,7 @@ object TestConstants {
           Features.RouteBlinding -> FeatureSupport.Optional,
           Features.StaticRemoteKey -> FeatureSupport.Mandatory,
           Features.Quiescence -> FeatureSupport.Optional,
-          Features.SplicePrototype -> FeatureSupport.Optional,
+          Features.Splicing -> FeatureSupport.Optional,
           Features.ProvideStorage -> FeatureSupport.Optional,
           Features.ChannelType -> FeatureSupport.Mandatory
         ),
@@ -295,7 +295,7 @@ object TestConstants {
         Features.StaticRemoteKey -> FeatureSupport.Mandatory,
         Features.AnchorOutputsZeroFeeHtlcTx -> FeatureSupport.Optional,
         Features.Quiescence -> FeatureSupport.Optional,
-        Features.SplicePrototype -> FeatureSupport.Optional,
+        Features.Splicing -> FeatureSupport.Optional,
         Features.ChannelType -> FeatureSupport.Mandatory
       ),
       pluginParams = Nil,
